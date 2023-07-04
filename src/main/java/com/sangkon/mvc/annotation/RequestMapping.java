8cf23fb95229dd89174301855020a03b0d54b0aa
Sangkon Han <sigmadream@gmail.com>
2023-07-05 02:07:59 +0900

    어노테이션 추가

---
+++ src/main/java/com/sangkon/mvc/annotation/RequestMapping.java
@@ -0,0 +1,16 @@
+package com.sangkon.mvc.annotation;
+
+import com.sangkon.mvc.controller.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMapping {
+    String value() default "";
+
+    RequestMethod[] method() default {};
+}
