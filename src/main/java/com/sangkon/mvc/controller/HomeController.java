8ddc86bcdb4494577492a291c8f7f8d4018e4e8b
Sangkon Han <sigmadream@gmail.com>
2023-07-05 02:09:07 +0900

    어노테이션을 활용한 컨트롤러

--- src/main/java/com/sangkon/mvc/controller/HomeController.java
+++ src/main/java/com/sangkon/mvc/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.sangkon.mvc.controller;
 
+import com.sangkon.mvc.annotation.Controller;
+import com.sangkon.mvc.annotation.RequestMapping;
 import com.sangkon.mvc.repository.UserRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeController implements Controller {
-    @Override
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
         request.setAttribute("users", UserRepository.findAll());
         return "home";
